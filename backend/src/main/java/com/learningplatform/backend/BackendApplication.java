3a5cdb652cd48d7a813f0a1ee45f2d510782a45c
adirm <adirm1609@gmail.com>
2025-07-13 23:19:15 +0800

    fixed deploy issues

--- backend/src/main/java/com/learningplatform/backend/BackendApplication.java
+++ backend/src/main/java/com/learningplatform/backend/BackendApplication.java
@@ -4,11 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableScheduling
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+
+        // Load all env vars from .env into system properties
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
